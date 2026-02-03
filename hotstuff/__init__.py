1389795f7593365948ef48758d4c3ef7e545bc7d
vyomshm <vyom@syndr.com>
2026-02-03 19:06:23 +0530

    version bump

--- hotstuff/__init__.py
+++ hotstuff/__init__.py
@@ -3,7 +3,7 @@
 A Python SDK for interacting with Hotstuff L1.
 """
 
-__version__ = "0.0.1-beta.3"
+__version__ = "0.0.1-beta.4"
 
 # Transports
 from hotstuff.transports import HttpTransport, WebSocketTransport
