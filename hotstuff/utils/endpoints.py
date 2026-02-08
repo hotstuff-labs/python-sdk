684ea53792a823d07a734ae62d06123efebca644
vaibhav2vd <vaibhav@syndr.com>
2026-02-09 00:15:42 +0530

    Update endpoints.py

--- hotstuff/utils/endpoints.py
+++ hotstuff/utils/endpoints.py
@@ -2,9 +2,9 @@
 
 ENDPOINTS_URLS = {
     "mainnet": {
-        "api": "https://testnet-api.hotstuff.trade/",
+        "api": "https://api.hotstuff.trade/",
         "rpc": "https://rpc.hotstuff.trade/",
-        "ws": "wss://testnet-api.hotstuff.trade/ws/",
+        "ws": "wss://api.hotstuff.trade/ws/",
     },
     "testnet": {
         "api": "https://testnet-api.hotstuff.trade/",
