a125290478d7b0078d0f0cde5bb6c665c13a9d1a
vaibhav-syndr <vaibhav@syndr.com>
2026-02-19 21:59:48 +0530

    funnier

--- examples/08_place_order.py
+++ examples/08_place_order.py
@@ -1,7 +1,7 @@
 """Example: Place orders."""
 import json
 import example_utils
-from hotstuff import PlaceOrderParams, UnitOrder
+from hotstuff import PlaceOrderParams, UnitOrder, BrokerConfig
 import time
 
 
@@ -11,10 +11,29 @@ def main():
     print("--------------------------------\nPlace orders\n")
     _, exchange = example_utils.setup_clients(is_testnet=True, main_account=False)
 
-    unit_order = UnitOrder(instrumentId=1, side="b", positionSide="BOTH", price="10000", size="1", tif="GTC", ro=False, po=False)
+    unit_order = UnitOrder(
+        instrumentId=1,
+        side="b",
+        positionSide="BOTH",
+        price='100',
+        size="0.01",
+        tif="GTC",
+        ro=False,
+        po=False,
+        # cloid='test-order-1',
+        # triggerPx='',
+        # isMarket=False,
+        # tpsl='',
+        # grouping='',
+    )
+
+    # broker_config = BrokerConfig(broker='', fee='0.001')
+
     expires_after = int(time.time() * 1000) + 3600000
+
+    place_order_params = PlaceOrderParams(orders=[unit_order], expiresAfter=expires_after)
     
-    result = exchange.place_order(PlaceOrderParams(orders=[unit_order], expiresAfter=expires_after))
+    result = exchange.place_order(place_order_params)
         
     print(f"Orders placed successfully!\n\nResponse: {json.dumps(result, indent=2)}\n--------------------------------\n")
         
